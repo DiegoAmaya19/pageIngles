7aa989caa5350e277b500e69249cfd67551a1efb
Diego Amaya <diegoamayatoro@gmail.com>
2024-08-13 11:22:18 -0500

    act7

--- src/app/clase-tenses/clase-tenses.component.ts
+++ src/app/clase-tenses/clase-tenses.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-clase-tenses',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterModule],
   templateUrl: './clase-tenses.component.html',
   styleUrl: './clase-tenses.component.css'
 })
