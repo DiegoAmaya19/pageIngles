a3e37591e694e9fcf57f7eebc705d30bc9e9ccef
Diego Amaya <diegoamayatoro@gmail.com>
2024-08-12 20:30:05 -0500

    act3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AporteCorreoComponent } from './aporte-correo/aporte-correo.component';
+import { ClaseTensesComponent } from './clase-tenses/clase-tenses.component';
 
 export const routes: Routes = [
     
-    {path: 'aporteCorreo', component: AporteCorreoComponent}
+    {path: 'aporteCorreo', component: AporteCorreoComponent},
+    {path: 'claseTenses', component: ClaseTensesComponent}
 
 ];
---
+++ src/app/clase-tenses/clase-tenses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clase-tenses',
+  standalone: true,
+  imports: [],
+  templateUrl: './clase-tenses.component.html',
+  styleUrl: './clase-tenses.component.css'
+})
+export class ClaseTensesComponent {
+
+}
