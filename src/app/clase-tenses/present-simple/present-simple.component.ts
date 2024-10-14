f9af47f7b88902d3c426d2b32684656be0b8338b
Diego Amaya <diegoamayatoro@gmail.com>
2024-10-14 16:53:58 -0500

    huh

--- src/app/clase-tenses/present-simple/present-simple.component.ts
+++ src/app/clase-tenses/present-simple/present-simple.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-present-simple',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, RouterLink],
   templateUrl: './present-simple.component.html',
   styleUrl: './present-simple.component.css'
 })
